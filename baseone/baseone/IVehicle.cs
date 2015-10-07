14da4331ecf5e978cca3cbbe654b75dfbf9a432f
unmeshM <visit2unmesh@gmail.com>
2015-10-07 22:37:50 +0530

    first commit
    commit first changes

---
+++ baseone/baseone/IVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baseone
+{
+    public interface IVehicle
+    {
+        int getVehicleId();
+        string getVehicleName();
+        string getDescription();
+    }
+}
---
+++ baseone/baseone/IVehicleRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baseone
+{
+    public interface IVehicleRepository<TVehicle> where TVehicle:IVehicle
+    {
+        void Add(TVehicle vehicle);
+        TVehicle GetVehicle(Func<TVehicle> query);
+        void Delete(TVehicle vehicle);
+        IQueryable<TVehicle> GetAllVehicles();
+    }
+}
